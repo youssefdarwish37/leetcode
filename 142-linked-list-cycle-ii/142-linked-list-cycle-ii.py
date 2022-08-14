027b234a1074e76ca2de1a45336e52758eff3618
youssef darwish <85325600+youssefdarwish37@users.noreply.github.com>
2022-08-14 11:55:43 +0200

    Time: 95 ms (29.10%), Space: 17.3 MB (94.43%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+#using slow and fast pointer slow moves by 1 and fast moves by 2
+#if slow reaches fast then then there is a loop as if there is no loop fast will reach a null before slow
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast =head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+            if fast==slow:
+                break
+        else:
+            return None \
+        #we don't have a cycle
+        
+        slow2=head
+        # they will meet beacuse of the previus loop pointer the always meet at the end of the loop
+        while slow2!=slow:
+            slow2=slow2.next
+            slow=slow.next
+        return slow2

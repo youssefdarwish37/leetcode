36f489a5ad0b16ff4d9e98cc8244597faa0d239f
youssef darwish <85325600+youssefdarwish37@users.noreply.github.com>
2022-08-17 14:46:29 +0200

    Time: 458 ms (20.46%), Space: 15.4 MB (73.46%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,12 +1,26 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        index=0
-        for i in nums:
-            if i == target:
-                return (index)
-                break
-            index+=1
-        else:
-            return -1
+        # index=0
+        # for i in nums:
+        #     if i == target:
+        #         return (index)
+        #         break
+        #     index+=1
+        # else:
+        #     return -1
+#         my solution was o(n)
+# lets solve another one in o(logn)
+        left_index=0
+        right_index=len(nums)-1
+        
+        while(left_index<=right_index):
+            mid=int((left_index+right_index)/2)
+            if nums[mid]>target:
+                right_index=mid - 1
+            elif nums[mid]<target:
+                left_index=mid+1
+            else:
+                return mid
+        return -1
             
         

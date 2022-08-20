25422ad4ce879e812500c5026f5fa4279af62748
youssef darwish <85325600+youssefdarwish37@users.noreply.github.com>
2022-08-20 12:45:41 +0200

    Time: 1435 ms (6.41%), Space: 13.9 MB (9.62%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def fib(self, n: int) -> int:
-        def fiba(n):
             if n==0 or n==1:
                 return n
             else:
-                return fiba((n-1)) + fiba((n-2))
-        return fiba(n)
+                return self.fib((n-1)) + self.fib((n-2))

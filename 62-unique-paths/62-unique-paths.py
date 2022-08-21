747170e366aceb9ea8450927eae22c99910b5782
youssef darwish <85325600+youssefdarwish37@users.noreply.github.com>
2022-08-21 14:45:25 +0200

    Time: 52 ms (42.51%), Space: 14 MB (32.97%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,5 +1,16 @@
-class Solution:
-    import math
-    def uniquePaths(self, m: int, n: int) -> int:
+# class Solution:
+#     import math
+#     def uniquePaths(self, m: int, n: int) -> int:
          
-        return int(math.factorial(m+n-2)/(math.factorial(m-1)*math.factorial(n-1)))
+#         return int(math.factorial(m+n-2)/(math.factorial(m-1)*math.factorial(n-1)))
+class Solution:
+
+     def uniquePaths(self, m: int, n: int) -> int:
+            lastrow=[1]*n
+            for i in range (m-1):
+                newrow=[1]*n
+                for j in range (n-2,-1,-1):
+                    newrow[j]=newrow[j+1]+lastrow[j]
+                lastrow =newrow
+            return lastrow[0]
+         
